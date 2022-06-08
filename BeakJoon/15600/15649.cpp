ddfb186ce20c7fbb6d84ab95acf5bf89004aa73d
unknown <aswoo55555@gmail.com>
2022-06-08 18:13:10 +0900

    백준 15649번 commit

---
+++ BeakJoon/15600/15649.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int m, n;
+int arr[8];
+int visited[8];
+
+void backTrack(int x) {
+    if (x >= n) {
+        for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+        printf("\n");
+        return;
+    }
+
+    for (int i = 0; i < m; i++)
+        if (!visited[i]) {
+            visited[i] = 1;
+            arr[x] = i + 1;
+            backTrack(x + 1);
+            visited[i] = 0;
+        }
+}
+
+int main(void) {
+    scanf("%d %d", &m, &n);
+
+    backTrack(0);
+
+    return 0;
+}
