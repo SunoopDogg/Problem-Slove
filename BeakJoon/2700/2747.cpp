06174c58d8d2ddc2f752fcea49b52fcb702a48e5
unknown <aswoo55555@gmail.com>
2022-09-26 11:40:45 +0900

    백준 2747번 commit

---
+++ BeakJoon/2700/2747.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+long long arr[46] = {0, 1};
+
+long long dp(int n) {
+    if (arr[n]) return arr[n];
+    if (n == 0) return 0;
+    return arr[n] = dp(n - 1) + dp(n - 2);
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    printf("%lld", dp(n));
+
+    return 0;
+}
