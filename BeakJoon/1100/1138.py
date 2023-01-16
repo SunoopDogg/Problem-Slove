facee297bf5773727dee4228ffba0db96ef5fd70
SunoopDogg <aswoo55555@gmail.com>
2023-01-16 13:04:04 +0900

    백준 1138번 commit

---
+++ BeakJoon/1100/1138.py
@@ -0,0 +1,14 @@
+N = int(input())
+l = list(map(int, input().split()))
+
+result = [0] * N
+for i in range(N):
+    count = 0
+    for j in range(N):
+        if count == l[i] and result[j] == 0:
+            result[j] = i+1
+            break
+        if result[j] == 0:
+            count += 1
+
+print(*result)
