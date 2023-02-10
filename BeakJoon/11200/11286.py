143b40dc93aafb0baefb49d86c3661704a97e014
SunoopDogg <aswoo55555@gmail.com>
2023-02-11 03:12:42 +0900

    백준 11286번 commit

---
+++ BeakJoon/11200/11286.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+N = int(sys.stdin.readline())
+heap = []
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (abs(x), x))
