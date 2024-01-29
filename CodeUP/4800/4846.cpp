91dc1dc0f21b793b9819442a5546da733db154fe
SunoopDogg <aswoo55555@gmail.com>
2024-01-29 20:24:37 +0900

    코드업 4846번 commit

---
+++ CodeUP/4800/4846.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, a, b;
+	int sum = 0;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &a, &b);
+		sum += b % a;
+	}
+
+	printf("%d", sum);
+
+	return 0;
+}
